1e797dc0d9e019280fb7b4d391ad47ff47ef0549
Nguyen Giang <nguyengiang08042003@gmail.com>
2024-05-07 13:57:31 +0700

    khai bao danh sach lien ket

--- BTLDSA.c
+++ BTLDSA.c
@@ -3,4 +3,21 @@ typedef struct node{
     char ten[30];
     int namsinh;
     int *cha;   
+    int *pre_chiEm;
+    int *next_chiEm;
+    int *pre_con;
+    int *next_con;
+    nguoi *link;
 }nguoi;
+typedef struct Danhsach{
+    nguoi *first;
+    nguoi *last;
+}list;
+void init(list *l){
+    l->first = NULL;
+    l->last = NULL;
+}
+
+int main(){
+    
+}
